e1d816640bf6ceaeec8a58a20a5952ed1c8dc699
Your Name <you@example.com>
2024-04-15 12:28:49 +0300

    crew page

--- space-tourism-multi-page/src/App.js
+++ space-tourism-multi-page/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Destination from "./pages/Destination";
-
+import Crew from "./pages/crew";
 function App() {
   return (
     <Router>      
@@ -10,6 +10,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
         </Routes>
       </div>
     </Router>
---
+++ space-tourism-multi-page/src/pages/crew.js
@@ -0,0 +1,97 @@
+import "./crew.css";
+import Navbar from "../components/navbar";
+import { useEffect, useState } from "react";
+import Data from "../data.json";
+import Douglas from "./assets/crew/image-douglas-hurley.png";
+import Mark from "./assets/crew/image-mark-shuttleworth.png";
+import Victor from "./assets/crew/image-victor-glover.png";
+import Anousheh from "./assets/crew/image-anousheh-ansari.png";
+
+const Crew = () => {
+  const [crew, setCrew] = useState(Data.crew);
+  const [position, setPosition] = useState(0);
+
+  console.log(crew[position]);
+
+  // console.log(crew);
+  useEffect(() => {
+    setCrew(Data.crew);
+  }, [crew]);
+
+  // useEffect(() => {
+  //   setposition(position)
+  // },[position])
+
+  function getdetails() {
+    return crew[position];
+  }
+  // console.log(Douglas);
+
+  function getcurrentimage() {
+    if (crew[position].name === "Douglas Hurley") {
+      return Douglas;
+    }
+    if (crew[position].name === "Mark Shuttleworth") {
+      return Mark;
+    }
+    if (crew[position].name === "Victor Glover") {
+      return Victor;
+    }
+    if (crew[position].name === "Anousheh Ansari") {
+      return Anousheh;
+    }
+  }
+
+  return (
+    <div className="destinationpage">
+      <Navbar />
+      <div className="maincontent">
+        <div className="heading">
+          <h5>
+            <b>01</b>MEET YOUR CREW
+          </h5>
+        </div>
+        <div className="subcontent">
+          <div className="crewdetails">
+            <h4 className="role">{getdetails().role}</h4>
+            <div className="details"><h3>{getdetails().name}</h3>
+            <p className="detailsparagraph">{getdetails().bio}</p></div>
+            <div className="btns">
+              <div
+                className="btn"
+                onClick={() => {
+                  setPosition(0);
+
+                }}
+              ></div>
+              <div
+                className="btn"
+                onClick={() => {
+                  setPosition(1);
+                }}
+              ></div>
+              <div
+                className="btn"
+                onClick={() => {
+                  setPosition(2);
+                }}
+              ></div>
+              <div
+                className="btn"
+                onClick={() => {
+                  setPosition(3);
+                }}
+              ></div>
+            </div>
+          </div>
+
+          <div className="image">
+            <img src={getcurrentimage()} alt={getdetails()} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Crew;
